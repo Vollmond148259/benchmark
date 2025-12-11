152d7681fb01316edd45ad690f9e5eed08d45595
Vollmond148259 <Vollmond148259@gmail.com>
2025-12-11 09:45:47 +0300

    small fixes

--- code-benchmark.js
+++ code-benchmark.js
@@ -17,8 +17,7 @@
     ru: {
       page_title:
         "JS Benchmark Playground — сравнить скорость кода в онлайн редакторе JavaScript",
-      ui_header_title:
-        "JS Benchmark Playground — JS playground и онлайн редактор кода",
+      ui_header_title: "JS Benchmark Playground",
       ui_header_subtitle:
         "Сравнивайте производительность двух вариантов JavaScript‑кода. Отредактируйте оба блока и нажмите <code>Запустить бенчмарк</code>, чтобы увидеть статистику. Также вы можете запустить бенчмарк с помощью сочетаний клавиш <code>Cmd+S</code> (macOS) или <code>Ctrl+S</code> (Windows/Linux).",
       ui_lang_switcher_label: "Язык",
@@ -82,8 +81,7 @@
     en: {
       page_title:
         "JS Benchmark Playground — JS playground & online JavaScript editor",
-      ui_header_title:
-        "JS Benchmark Playground — JS playground & online editor",
+      ui_header_title: "JS Benchmark Playground",
       ui_header_subtitle:
         "Compare performance of two JavaScript code variants. Edit both blocks and press <code>Run benchmark</code> to see statistics. You can also run the benchmark with <code>Cmd+S</code> (macOS) or <code>Ctrl+S</code> (Windows/Linux).",
       ui_lang_switcher_label: "Language",
